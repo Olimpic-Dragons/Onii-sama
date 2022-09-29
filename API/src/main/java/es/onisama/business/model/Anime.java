dfa024b0adf7b9383ad17c278b22c43c768558ae
Willy1123 <guillermoblascoperez@gmail.com>
2022-09-29 16:33:37 +0200

    Quitados los Getters y Setters de Anime.java y sustituidos por el @Getter y @Setter del lombok. Añadido el Tipo "ESPECIAL" en TipoEnum.java

--- API/src/main/java/es/onisama/business/model/Anime.java
+++ API/src/main/java/es/onisama/business/model/Anime.java
@@ -3,6 +3,8 @@ package es.onisama.business.model;
 import es.onisama.business.model.enumerated.GenerosEnum;
 import es.onisama.business.model.enumerated.StatusEnum;
 import es.onisama.business.model.enumerated.TipoEnum;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -12,6 +14,7 @@ import java.util.*;
 
 @Entity
 @Table(name = "animes")
+@Getter @Setter
 public class Anime implements Serializable {
 
     @Serial
@@ -55,101 +58,6 @@ public class Anime implements Serializable {
     @Enumerated(EnumType.STRING)
     private Collection<GenerosEnum> generos;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Date getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(Date fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public Date getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(Date fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public int getCapitulosEmitidos() {
-        return capitulosEmitidos;
-    }
-
-    public void setCapitulosEmitidos(int capitulosEmitidos) {
-        this.capitulosEmitidos = capitulosEmitidos;
-    }
-
-    public int getCapitulosTotales() {
-        return capitulosTotales;
-    }
-
-    public void setCapitulosTotales(int capitulosTotales) {
-        this.capitulosTotales = capitulosTotales;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public String getUrlAflv() {
-        return urlAflv;
-    }
-
-    public void setUrlAflv(String urlAflv) {
-        this.urlAflv = urlAflv;
-    }
-
-    public StatusEnum getStatusEnum() {
-        return statusEnum;
-    }
-
-    public void setStatusEnum(StatusEnum statusEnum) {
-        this.statusEnum = statusEnum;
-    }
-
-    public TipoEnum getTipoEnum() {
-        return tipoEnum;
-    }
-
-    public void setTipoEnum(TipoEnum tipoEnum) {
-        this.tipoEnum = tipoEnum;
-    }
-
-    public Collection<GenerosEnum> getGeneros() {
-        return generos;
-    }
-
-    public void setGeneros(Collection<GenerosEnum> generos) {
-        this.generos = generos;
-    }
 
     @Override
     public boolean equals(Object o) {
