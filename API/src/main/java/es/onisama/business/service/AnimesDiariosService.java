63e3cb906efe5ca1c46217d65a94c35da5187bf1
DGuttman <34478712+DGuttmanSN@users.noreply.github.com>
2022-09-29 09:01:38 +0200

    Servicio para obtener los animes diarios creado.

---
+++ API/src/main/java/es/onisama/business/service/AnimesDiariosService.java
@@ -0,0 +1,9 @@
+package es.onisama.business.service;
+
+import java.util.List;
+
+public interface AnimesDiariosService {
+
+    public List<Anime> getAnimesDiarios();
+
+}
---
+++ API/src/main/java/es/onisama/business/service/impl/AnimesDiariosServiceImpl.java
@@ -0,0 +1,14 @@
+package es.onisama.business.service.impl;
+
+import es.onisama.business.service.AnimesDiariosService;
+
+import java.util.List;
+
+public class AnimesDiariosServiceImpl implements AnimesDiariosService {
+
+    @Override
+    public List<Anime> getAnimesDiarios() {
+        return null;
+    }
+
+}
