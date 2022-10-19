4f0fe42b3869d018e9a78d46045578cc19d259e2
Willy1123 <guillermoblascoperez@gmail.com>
2022-10-20 00:15:28 +0200

    Cambiado a typescript en vez de javascript, creada la PlantillaEmbedHTTPRequest para usarla en un futuro, ya que está dando fallos. Empezaré primero por crear mensajes embeds a mano y ya cuando funcione implementamos el HTTPRequest

--- JS/index.ts
+++ JS/index.ts
@@ -1,16 +1,18 @@
-
 const Discord = require("discord.js");
 const intents = new Discord.Intents();
 const config = require("./config.json");
+const {request} = require('undici');
+const paginationEmbed = require('discordjs-button-pagination');
+const {MessageEmbed} = require('discord.js');
 
 // CREA EL CLIENTE
-const client = new Discord.Client({ intents: 32767 });
+const client = new Discord.Client({intents: 32767});
 
 // SE EJECUTA CUANDO SE INICIA EL BOT
 client.on('ready', () => console.log("Estoy listo"));
 
 const fs = require("fs");
-let { readdirSync } = require("fs");
+let {readdirSync} = require("fs");
 
 // Para manejar los comando con prefijo. Lo dejo por si algún día lo usamos
 client.commands = new Discord.Collection();
@@ -24,18 +26,20 @@ for (const file of commandsFiles) {
 client.slashcommands = new Discord.Collection();
 const slashcommandsFiles = fs.readdirSync("./Commands").filter(file => file.endsWith("js"));
 
-for(const file of slashcommandsFiles) {
+for (const file of slashcommandsFiles) {
     //const commandName = file.split(".")[0];
     const slash = require(`./Commands/${file}`);
     console.log(`Slash commands - ${file} cargado.`);
     client.slashcommands.set(slash.data.name, slash);
-};
+}
+;
 
-client.on("interactionCreate", async(interaction) => {
+client.on("interactionCreate", async (interaction) => {
+    /**
+     * Majejo de slashcommands
+     */
     if (!interaction.isCommand()) return;
-
     const slashcmds = client.slashcommands.get(interaction.commandName);
-
     if (!slashcmds) return;
 
     try {
@@ -43,29 +47,10 @@ client.on("interactionCreate", async(interaction) => {
         console.log("Interaction creada!")
     } catch (e) {
         console.error(e);
-        console.log("Error al crear la interaction!")
+        console.log("Error al crear la interaction!");
+        throw e;
     }
 
-});
-
-
-// Para manejar los comandos con prefijo. lo dejo por si algún día lo usamos.
-client.on("messageCreate", (message) => {
-    if(message.content.startsWith(message)) {
-        let prefix = ".";
-
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-
-        let usuario = message.mentions.members.first() || message.member;
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const commandName = args.shift().toLowerCase();
-
-        const cmd = client.commands.find((c) => c.name === command || c.alias && c.alias.includes(command))
-        if (cmd) {
-            cmd.execute(client, message, args);
-        }
-    }
 });
 
 client.login(config.BOT_TOKEN)
