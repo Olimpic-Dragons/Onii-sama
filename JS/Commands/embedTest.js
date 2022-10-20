64a7244a70f73441c61405f1112930e8e1b8b3ff
Willy1123 <guillermoblascoperez@gmail.com>
2022-10-20 06:32:35 +0200

    Función de Paginación terminada con éxito.

--- JS/Commands/embedTest.js
+++ JS/Commands/embedTest.js
@@ -10,17 +10,17 @@ module.exports = {
 
     async run(client, interaction) {
         const embed1 = new MessageEmbed()
-            .setColor(0x0099ff)
+            .setColor(0x7CF95C)
             .setTitle("Anime 1")
             .setDescription("Descripción del anime 1");
 
         const embed2 = new MessageEmbed()
-            .setColor(0x0099ff)
+            .setColor(0x5CF4F9)
             .setTitle("Anime 2")
             .setDescription("Descripción del anime 2");
 
         const embed3 = new MessageEmbed()
-            .setColor(0x0099ff)
+            .setColor(0xF95C5C)
             .setTitle("Anime 3")
             .setDescription("Descripción del anime 3");
 
