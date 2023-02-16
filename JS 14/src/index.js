185197bb47022329b4d048f34d13d06072e507ed
Willy1123 <guillermoblascoperez@gmail.com>
2023-02-16 03:43:10 +0100

    Empezada la actualización a la versión de discordJs 14. Comando "/ping" funcionando - Falta actualizar los demás comandos

---
+++ JS 14/src/index.js
@@ -0,0 +1,44 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const config = require("../config.json");
+
+// CREA EL CLIENTE
+const client = new Client({intents: 32767});
+
+// SE EJECUTA CUANDO SE INICIA EL BOT
+client.on('ready', () => console.log("Estoy listo"));
+
+/**
+ * Manejo de slashcommands
+ */
+const fs = require("fs");
+let {readdirSync} = require("fs");
+
+client.slashCommands = new Collection();
+const slashCommandsFiles = fs
+    .readdirSync("./src/comandos")
+    .filter(file => file.endsWith("js"));
+
+for (const file of slashCommandsFiles) {
+    const slash = require(`./comandos/${file}`);
+    console.log(`Slash commands - ${file} cargado.`);
+    client.slashCommands.set(slash.data.name, slash);
+}
+
+client.on("interactionCreate", async (interaction) => {
+
+    if (!interaction.isCommand()) return;
+    const slashCommands = client.slashCommands.get(interaction.commandName);
+    if (!slashCommands) return;
+
+    try {
+        await slashCommands.run(client, interaction);
+        console.log("Interaction creada!")
+    } catch (e) {
+        console.error(e);
+        console.log("Error al crear la interaction!");
+        throw e;
+    }
+
+});
+
+client.login(config.BOT_TOKEN)
---
+++ JS 14/src/utils/createCommands.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { REST, Routes } = require("discord.js");
+const config = require("../../config.json");
+const  commands = [];
+const slashCommandsFiles = fs.readdirSync("./src/comandos").filter((file) => file.endsWith("js"));
+
+
+for (const file of slashCommandsFiles) {
+    const slash = require(`../comandos/${file}`);
+    commands.push(slash.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(config.BOT_TOKEN);
+
+createSlash()
+
+async function createSlash() {
+    try{
+        await rest.put(
+            //quitando el GUILD_ID serviría de forma global
+            Routes.applicationCommands(config.BOT_ID, config.GUILD), {
+                body: commands
+            }
+        )
+        console.log("[Slash commands] agregados.");
+    } catch (e) {
+        console.error(e);
+        console.log("Fallo al agregar los Slash commands.");
+    }
+}
